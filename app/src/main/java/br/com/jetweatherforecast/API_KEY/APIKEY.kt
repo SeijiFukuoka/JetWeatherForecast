a312bd1981341d3b14f254ccb15c67c78b975bfd
Seiji <seiji@skeelo.app>
2022-09-28 20:03:20 -0300

    feat: add start structure

---
+++ app/src/main/java/br/com/jetweatherforecast/API_KEY/APIKEY.kt
@@ -0,0 +1,7 @@
+package br.com.jetweatherforecast.API_KEY
+
+object APIKEY {
+    /*
+    https://api.openweathermap.org/data/2.5/forecast/daily?q=lisbon&appid=ed60fcfbd110ee65c7150605ea8aceea&units=imperial
+     */
+}
---
+++ app/src/main/java/br/com/jetweatherforecast/di/AppModule.kt
@@ -0,0 +1,5 @@
+package br.com.jetweatherforecast.di
+
+class AppModule {
+
+}
