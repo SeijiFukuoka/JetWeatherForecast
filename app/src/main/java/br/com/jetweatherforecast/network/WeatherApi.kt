678285ebf51a071af23422fa7d1c9f6378360f96
Seiji <seiji@skeelo.app>
2022-09-28 21:38:59 -0300

    feat: create weather api

---
+++ app/src/main/java/br/com/jetweatherforecast/network/WeatherApi.kt
@@ -0,0 +1,17 @@
+package br.com.jetweatherforecast.network
+
+import br.com.jetweatherforecast.model.Weather
+import br.com.jetweatherforecast.utils.Constants
+import retrofit2.http.GET
+import retrofit2.http.Query
+import javax.inject.Singleton
+
+@Singleton
+interface WeatherApi {
+    @GET(value = "data/2.5/forecast/daily")
+    suspend fun getWeather(
+        @Query("q") query: String,
+        @Query("units") units: String = "imperial",
+        @Query("appid") appId: String = Constants.API_KEY
+    ): Weather
+}
