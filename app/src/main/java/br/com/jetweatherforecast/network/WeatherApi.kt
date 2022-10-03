3e0c2a6d362eac576326eec21ea75dd4a9bf9139
Seiji <seiji@skeelo.app>
2022-10-03 19:43:30 -0300

    feat: add app module and retrofit service

--- app/src/main/java/br/com/jetweatherforecast/network/WeatherApi.kt
+++ app/src/main/java/br/com/jetweatherforecast/network/WeatherApi.kt
@@ -8,10 +8,12 @@ import javax.inject.Singleton
 
 @Singleton
 interface WeatherApi {
+
     @GET(value = "data/2.5/forecast/daily")
     suspend fun getWeather(
         @Query("q") query: String,
         @Query("units") units: String = "imperial",
         @Query("appid") appId: String = Constants.API_KEY
     ): Weather
+
 }
