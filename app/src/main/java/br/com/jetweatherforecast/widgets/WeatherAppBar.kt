dfea59ab8a0751390c40ebd235d5a99bad7a9423
Seiji <seiji@skeelo.app>
2022-11-07 22:05:06 -0300

    feat: add favorite verification at main screen

--- app/src/main/java/br/com/jetweatherforecast/widgets/WeatherAppBar.kt
+++ app/src/main/java/br/com/jetweatherforecast/widgets/WeatherAppBar.kt
@@ -1,5 +1,7 @@
 package br.com.jetweatherforecast.widgets
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -16,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -47,6 +50,12 @@ fun WeatherAppBar(
     if (showDialog.value) {
         ShowSettingDropDownMenu(showDialog = showDialog, navController = navController)
     }
+
+    val showIt = remember {
+        mutableStateOf(false)
+    }
+
+    val context = LocalContext.current
 
     TopAppBar(
         title = {
@@ -88,28 +97,50 @@ fun WeatherAppBar(
                     })
             }
             if (isMainScreen) {
-                Icon(
-                    imageVector = Icons.Default.Favorite,
-                    contentDescription = "Favorite Icon",
-                    modifier = Modifier
-                        .scale(0.9f)
-                        .clickable {
-                            val dataList = title.split(",")
-                            favoriteViewModel
-                                .insertFavorite(
-                                    favorite = Favorite(
-                                        city = dataList[0], // city name
-                                        country = dataList[1] // country code
+
+                val isAlreadyFavList = favoriteViewModel
+                    .favList.collectAsState().value.filter { item ->
+                        (item.city == title.split(",").first())
+                    }
+
+                if (isAlreadyFavList.isEmpty()) {
+                    Icon(
+                        imageVector = Icons.Default.Favorite,
+                        contentDescription = "Favorite Icon",
+                        modifier = Modifier
+                            .scale(0.9f)
+                            .clickable {
+                                val dataList = title.split(",")
+                                favoriteViewModel
+                                    .insertFavorite(
+                                        favorite = Favorite(
+                                            city = dataList[0], // city name
+                                            country = dataList[1] // country code
+                                        )
                                     )
-                                )
-                        },
-                    tint = Color.Red.copy(alpha = 0.6f)
-                )
+                                    .run {
+                                        showIt.value = true
+                                    }
+                            },
+                        tint = Color.Red.copy(alpha = 0.6f)
+                    )
+                } else {
+                    showIt.value = false
+                    Box {}
+                }
+                ShowToast(context = context, showIt)
             }
         },
         backgroundColor = Color.Transparent,
         elevation = elevation
     )
+}
+
+@Composable
+fun ShowToast(context: Context, showIt: MutableState<Boolean>) {
+    if (showIt.value) {
+        Toast.makeText(context, "Added to Favorites", Toast.LENGTH_SHORT).show()
+    }
 }
 
 @Composable
